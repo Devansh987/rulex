3d12108e3a357df68d0fd8617a5797bc7aebaa85
Devansh Tomar <d98291479@gmail.com>
2026-01-10 23:24:27 +0530

    Initial commit - RuleX project setup

---
+++ src/main/java/com/rulex/rulex/RulexApplication.java
@@ -0,0 +1,13 @@
+package com.rulex.rulex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RulexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RulexApplication.class, args);
+	}
+
+}
