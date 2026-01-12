07c542e9b12e33bc5b65db6a8371d5f6ca3dd8e7
Devansh Tomar <d98291479@gmail.com>
2026-01-12 22:53:04 +0530

    Conecting to Database supabase

--- src/main/java/com/rulex/rulex/Entity/Policy.java
+++ src/main/java/com/rulex/rulex/Entity/Policy.java
@@ -1,4 +1,12 @@
 package com.rulex.rulex.Entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+@Entity
+@Data
+@Table(name = "Policy")
 public class Policy {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
--- src/main/java/com/rulex/rulex/Entity/Tenent.java
+++ src/main/java/com/rulex/rulex/Entity/Tenent.java
@@ -1,4 +1,12 @@
 package com.rulex.rulex.Entity;
 
+
+import jakarta.persistence.*;
+
+@Entity
 public class Tenent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
