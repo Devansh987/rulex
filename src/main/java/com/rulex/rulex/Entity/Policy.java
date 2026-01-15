aa7527bf82ccfa00cde83a78f6a276e755133610
Devansh Tomar <d98291479@gmail.com>
2026-01-15 23:49:25 +0530

    Entities

--- src/main/java/com/rulex/rulex/Entity/Policy.java
+++ src/main/java/com/rulex/rulex/Entity/Policy.java
@@ -2,11 +2,22 @@ package com.rulex.rulex.Entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+
+import java.time.LocalDateTime;
+
 @Entity
 @Data
 @Table(name = "Policy")
 public class Policy {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+
     private Long id;
+    private String policyCode;
+    private String description;
+    private String Status;
+    private Tenant tenant;
+    private LocalDateTime created_at;
+
 }
--- src/main/java/com/rulex/rulex/Entity/Rules.java
+++ src/main/java/com/rulex/rulex/Entity/Rules.java
@@ -1,4 +1,10 @@
 package com.rulex.rulex.Entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "Rules")
 public class Rules {
 }
