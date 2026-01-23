f52510280450c41e982777d71e8c81c4aa2ce948
Devansh Tomar <d98291479@gmail.com>
2026-01-23 11:23:13 +0530

    Added Lombok Annotation

--- src/main/java/com/rulex/rulex/Entity/Policy.java
+++ src/main/java/com/rulex/rulex/Entity/Policy.java
@@ -5,9 +5,10 @@ package com.rulex.rulex.Entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
 import java.time.LocalDateTime;
-
+@Data
 @Entity
 @Table(
         name = "policies",
