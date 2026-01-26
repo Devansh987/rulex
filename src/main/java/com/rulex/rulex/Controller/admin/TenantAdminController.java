523ed2bd48a7cea8627b3512b54961e695f523fe
Devansh Tomar <d98291479@gmail.com>
2026-01-27 00:57:11 +0530

    Fully implemented tenant controller

--- src/main/java/com/rulex/rulex/Controller/admin/TenantAdminController.java
+++ src/main/java/com/rulex/rulex/Controller/admin/TenantAdminController.java
@@ -10,15 +10,34 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("admin/tenant")
-@ControllerAdvice
 public class TenantAdminController {
 
     @Autowired
     private TenantService tenantService;
 
-    @PostMapping("/createTenant")
+    @PostMapping
     public ResponseEntity<Tenant> createTenant(@RequestBody Tenant tenant){
        Tenant saved =  tenantService.createTenant(tenant);
         return new ResponseEntity<>(saved,HttpStatus.CREATED);
     }
+
+    @GetMapping("/{tenantCode}")
+    public ResponseEntity<Tenant> getByTenantCode(@PathVariable String tenantCode){
+        Tenant got = tenantService.getTenantByCode(tenantCode);
+        return new ResponseEntity<>(got,HttpStatus.FOUND);
+    }
+
+    @PutMapping("/{tenantCode}")
+    public ResponseEntity<Tenant> updateTenant(@RequestBody Tenant updatedTenant,@PathVariable String tenantCode){
+        Tenant updated = tenantService.updateTenant(tenantCode,updatedTenant);
+        return new ResponseEntity<>(updated,HttpStatus.OK);
+    }
+
+
+    @DeleteMapping("/{tenantCode}")
+    public ResponseEntity<Void> disableTenant(@PathVariable String tenantCode) {
+        tenantService.disableTenant(tenantCode);
+        return ResponseEntity.noContent().build();
+    }
+
 }
