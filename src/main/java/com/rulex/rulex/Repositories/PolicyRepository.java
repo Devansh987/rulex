c3b3e71d45717ade9f18f1bab3f2e9a3f558af6f
Devansh Tomar <d98291479@gmail.com>
2026-01-28 01:06:17 +0530

    add custom method in repo

--- src/main/java/com/rulex/rulex/Repositories/PolicyRepository.java
+++ src/main/java/com/rulex/rulex/Repositories/PolicyRepository.java
@@ -3,7 +3,11 @@ package com.rulex.rulex.Repositories;
 import com.rulex.rulex.Entity.Policy;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PolicyRepository extends JpaRepository<Policy,Long> {
 
     Policy findByTenantIdAndPolicyCode(Long tenantCode ,String PolicyCode);
+
+    List<Policy> findAllByTenantId(Long tenantID);
 }
