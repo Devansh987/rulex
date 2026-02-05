86ca5715a32af73f55cca62e8ba8c1776d22cb99
Devansh Tomar <d98291479@gmail.com>
2026-02-05 23:32:01 +0530

    Implemented Policy Not Found Exception

---
+++ src/main/java/com/rulex/rulex/Exception/CustomException/PolicyNotFound.java
@@ -0,0 +1,7 @@
+package com.rulex.rulex.Exception.CustomException;
+
+public class PolicyNotFound  extends  RuntimeException{
+    public PolicyNotFound(String message) {
+        super(message);
+    }
+}
